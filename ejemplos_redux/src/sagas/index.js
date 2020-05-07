a24b9cc82fbca37e9411047e405a62989c673ee2
Mirka Monzón <35476538+MirkaNicolle@users.noreply.github.com>
2020-05-06 20:35:56 -0600

    .

--- ejemplos_redux/src/sagas/index.js
+++ ejemplos_redux/src/sagas/index.js
@@ -1,23 +1,21 @@
-import { fork, all } from 'redux-saga/effects';
+import { fork, all } from "redux-saga/effects";
 
-import { watchLoginStarted } from './auth';
-import { watchSayHappyBirthday } from './happyBirthday';
+import { watchLoginStarted } from "./auth";
+import { watchSayHappyBirthday } from "./happyBirthday";
 import {
-  watchPetOwnersFetch,
+  watchFetchOwners,
   watchAddPetOwner,
   watchRemovePetOwner,
-} from './petOwners';
-
+} from "./petOwners";
 
 function* mainSaga() {
   yield all([
     fork(watchLoginStarted),
     fork(watchSayHappyBirthday),
-    fork(watchPetOwnersFetch),
+    fork(watchFetchOwners),
     fork(watchAddPetOwner),
     fork(watchRemovePetOwner),
   ]);
 }
-
 
 export default mainSaga;
